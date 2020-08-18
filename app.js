95c26e873430e8139a5bfdec2b2f705bd9e4b179
友造-2525 <bathbook1990tomozou@gmail.com>
2020-08-18 18:48:42 +0900

    feature/v-onイベント実装

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let app = new Vue({
-	el: "#app",
-	data: {
-	  todos: [
-	  	"todo 1",
-	  	"todo 2",
-	  	"todo 3"
-	  ]
-	}
-})
+  el: "#app",
+  data: {
+    new_item: " ",
+    todos: ["todo 1", "todo 2", "todo 3"]
+  },
+  methods: {
+    addItem: function() {
+      this.todos.push(this.newItem);
+    }
+  }
+});
