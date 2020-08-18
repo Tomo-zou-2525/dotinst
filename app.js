2b9f2b6b397cbe73032f2ab6d71d0851a3b1d602
友造-2525 <bathbook1990tomozou@gmail.com>
2020-08-18 20:41:17 +0900

    feature/削除機能実装まで

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 let app = new Vue({
   el: "#app",
   data: {
-    new_item: " ",
+    new_item: "",
     todos: ["todo 1", "todo 2", "todo 3"]
   },
   methods: {
     addItem: function() {
       this.todos.push(this.newItem);
+      this.newItem = "";
+    },
+    deleteItem: function(index) {
+      this.todos.splice(index, 1);
+      this.newItem = "";
     }
   }
 });
