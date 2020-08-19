ab0bd4c6cb821a7a59d9b3de4aa70ad22c7463ce
友造-2525 <bathbook1990tomozou@gmail.com>
2020-08-19 12:41:24 +0900

    todo・全体数の表示

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ let app = new Vue({
   el: "#app",
   data: {
     newItem: "",
-    todos: ["todo 1", "todo 2", "todo 3"]
+    todos: [
+      { title: "task 1", isDone: false },
+      { title: "task 2", isDone: false },
+      { title: "task 3", isDone: true }
+    ]
   },
   methods: {
     addItem: function() {
-      this.todos.push(this.newItem);
-      this.newItem = "";
+      let item = {
+        title: this.newItem,
+        isDone: false
+      };
+      this.todos.push(item);
     },
     deleteItem: function(index) {
       if (confirm("消す？ 消しちゃう？")) {
         this.todos.splice(index, 1);
       }
+    }
+  },
+  computed: {
+    r: function() {
+      let items = this.todos.filter(function(todo) {
+        return !todo.isDone;
+      });
+      return items.length;
     }
   }
 });
